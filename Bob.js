3ad07c7b9e8a3c6b3dac25994575486d0e298663
hrit-lab <75931726+hrit-lab@users.noreply.github.com>
2021-02-06 15:08:52 +0530

    Add files via upload

--- Bob.js
+++ Bob.js
@@ -1,6 +1,7 @@
 class Bob{
     constructor(x,y){
         var options={
+            isStatic:true,
             'restitution':0,
             'friction':1,
             'density':1.2
--- Roof.js
+++ Roof.js
@@ -1,6 +1,7 @@
 class Roof{
     constructor(x,y,width,height){
         var options={
+            isStatic : true,
             'restitution':0,
             'friction':1,
             'density':1.2
